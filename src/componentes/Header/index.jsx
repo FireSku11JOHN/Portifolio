2fd7eb262d38ef0af43884b4bb3b8e147c6cd7a6
John Ávylan <avylanjohn@gmail.com>
2025-11-10 13:54:52 -0300

    Criado componentes Header e ButtonHeader

--- src/componentes/Header/index.jsx
+++ src/componentes/Header/index.jsx
@@ -7,25 +7,31 @@ export const Header = () => {
     const normalButtonStyles = "bg-bg-button-header text-center text-ligth text-[14px] leading-[120%] tracking-[1px] py-2 rounded flex-1"
 
     return (
-        <nav className="flex gap-3 mx-6 my-3">
-            <img src={Logo} className="bg-bg-logo h-8 py-2 rounded inline-block flex-1 object-contain"></img>
-
-            <ButtonHeader className={normalButtonStyles}>
-                HOME
-            </ButtonHeader>
-            <ButtonHeader className={normalButtonStyles}>
-                SOBRE
-            </ButtonHeader>
-            <ButtonHeader className={normalButtonStyles}>
-                SKILLS
-            </ButtonHeader>
-            <ButtonHeader className={normalButtonStyles}>
-                PROJETOS
-            </ButtonHeader>
-
-            <ButtonHeader className="bg-ligth-cyan text-center text-dark text-[14px] leading-[120%] tracking-[1px] py-2 rounded flex-1">
-                CONTATOS
-            </ButtonHeader>
+        <nav>
+            <div className="flex gap-3 mx-6 my-3 xs:hidden">
+                <img src={Logo} className="bg-bg-logo h-8 py-2 rounded inline-block flex-1 object-contain"></img>
+                <ButtonHeader className="bg-ligth-cyan text-center text-dark text-[14px] leading-[120%] tracking-[1px] py-2 rounded flex-1">
+                    CONTATOS
+                </ButtonHeader>
+            </div>
+            <div className="flex gap-3 mx-6 my-3">
+                <img src={Logo} className="bg-bg-logo h-8 py-2 rounded inline-block flex-1 object-contain max-xs:hidden"></img>
+                <ButtonHeader className={normalButtonStyles}>
+                    HOME
+                </ButtonHeader>
+                <ButtonHeader className={normalButtonStyles}>
+                    SOBRE
+                </ButtonHeader>
+                <ButtonHeader className={normalButtonStyles}>
+                    SKILLS
+                </ButtonHeader>
+                <ButtonHeader className={normalButtonStyles}>
+                    PROJETOS
+                </ButtonHeader>
+                <ButtonHeader className="bg-ligth-cyan text-center text-dark text-[14px] leading-[120%] tracking-[1px] py-2 rounded flex-1 max-xs:hidden">
+                    CONTATOS
+                </ButtonHeader>
+            </div>
         </nav>
     )
 }
