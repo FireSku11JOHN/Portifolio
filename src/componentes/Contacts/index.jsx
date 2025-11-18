85c3fa735e14fde8c0c784c672aba1bc4ec4c98a
John Avylan <avylanjohn@gmail.com>
2025-11-18 20:46:44 -0300

    Resppnsividade no componente Contacts

--- src/componentes/Contacts/index.jsx
+++ src/componentes/Contacts/index.jsx
@@ -1,44 +1,52 @@
 import bgGridContacts from "../../assets/img/bgGridContacts.png"
 import notebookBaloon from "../../assets/img/notebook-baloon.png"
 import IconGitHubContacts from "../../assets/Icons/IconGitHubContacts.svg"
+import IconCopy from "../../assets/Icons/IconCopy.svg"
+
+const DEFAULT_STYLE_CARD = "flex flex-col py-20 px-6 items-center bg-bg-card-contacts w-full max-w-[320px] border border-light rounded-lg gap-10 max-desktop:py-14 max-desktop:px-4 max-mobile:py-4 max-mobile:gap-2"
+
+const DEFAULT_STYLE_TITLE_CARD = "text-[22px] font-medium max-mobile:text-[16px]"
+
+const DEFAULT_STYLE_BUTTON_CARD = "text-[18px] font-medium text-dark bg-light py-4 px-6 w-fit rounded-[10px] max-tablet:text-[14px] max-tablet:py-2 max-mobile:w-full max-mobile:px-2"
 
 export const Contacts = () => {
     return (
         <div
-            className="text-light p-16 flex flex-col gap-12 relative "
+            className="text-light p-16 flex flex-col gap-12 relative text-center max-tablet:p-6"
             style={{
                 backgroundImage: `url(${bgGridContacts})`
                 ,
                 backgroundSize: "cover", backgroundPosition: "bottom center"
             }}
         >
-            <h2 className="text-[32px] font-bold">Contatos</h2>
+            <h2 className="text-[32px] font-bold z-5">Contatos</h2>
 
-            <div className="flex justify-evenly z-5">
-                <div className="flex flex-col py-20 px-18 items-center bg-bg-card-contacts w-fit border border-light rounded-lg gap-10">
+            <div className="flex justify-evenly z-5 max-desktop:justify-between gap-6">
+                <div className={DEFAULT_STYLE_CARD}>
                     <img src={IconGitHubContacts} alt="Icone GitHub"
                         className="w-40"
                     />
-                    <p className="text-[22px] font-bold">
+                    <p className={DEFAULT_STYLE_TITLE_CARD}>
                         GitHub
                     </p>
                     <a
                         href=""
-                        className="text-[18px] font-medium text-dark bg-light py-4 px-6 w-fit rounded-[10px]"
+                        className={DEFAULT_STYLE_BUTTON_CARD}
                     >
                         Ir para site
                     </a>
                 </div>
-                <div className="flex flex-col py-20 px-18 items-center bg-bg-card-contacts w-fit border border-light rounded-lg gap-10">
+
+                <div className={DEFAULT_STYLE_CARD}>
                     <img src={IconGitHubContacts} alt="Icone GitHub"
                         className="w-40"
                     />
-                    <p className="text-[22px] font-bold">
+                    <p className={DEFAULT_STYLE_TITLE_CARD }>
                         GitHub
                     </p>
                     <a
                         href=""
-                        className="text-[18px] font-medium text-dark bg-light py-4 px-6 w-fit rounded-[10px]"
+                        className={DEFAULT_STYLE_BUTTON_CARD}
                     >
                         Ir para site
                     </a>
@@ -46,13 +54,43 @@ export const Contacts = () => {
             </div>
 
             <div
-                className="w-[1040px] bg-linear-to-r from-email-dark-blue to-email-blue py-10 z-5 border border-light rounded-lg self-center"
+                className="flex flex-wrap gap-6 justify-center w-[80%] bg-linear-to-r from-email-dark-blue to-email-blue p-10 z-5 border border-light rounded-lg self-center max-desktop:w-full max-mobile:p-4"
             >
-                <div className="flex flex-col gap-8 w-fit py-3 px-2 rounded-lg bg-linear-to-r from-email-light-blue to-email-sky-blue">
-                    <h4 className="text-[22px] font-bold">Email</h4>
-                    <div className="bg-light text-dark text-[20px] py-1 px-3 rounded-[10px]">
+
+                <div className="flex flex-col h-fit w-[500px] text-start gap-8 py-3 px-2 rounded-lg bg-linear-to-r from-email-light-blue to-email-sky-blue">
+                    <h4 className="text-[22px] font-bold max-mobile:text-[16px]">
+                        Email
+                    </h4>
+                    <div className="flex justify-between gap-2 bg-light text-dark text-[20px] py-1 px-3 rounded-[10px] max-mobile:text-[16px]">
                         <p>avylanjohn@gmail.com</p>
+                        <button>
+                            <img src={IconCopy} alt="Copiar"
+                            className=""
+                            />
+                        </button>
                     </div>
+                </div>
+
+                <div className="flex flex-col gap-6 text-start w-[500px] ">
+                    <label
+                        className="text-[22px] font-bold max-mobile:text-[16px]"
+                        htmlFor="msg">
+                        Envie uma menssagem
+                    </label>
+                    <textarea
+                        name="msg"
+                        id="msg"
+                        placeholder="Escreva sua mensagem"
+                        className="h-[200px] bg-light rounded-[10px] text-left text-dark p-3 resize-none focus:outline-hidden max-mobile:text-[16px]"
+                    >
+
+                    </textarea>
+
+                    <button
+                        className="w-fit text-[16px] text-dark font-medium bg-light rounded-[10px] py-2 px-4"
+                    >
+                        Enviar
+                    </button>
                 </div>
             </div>
 
