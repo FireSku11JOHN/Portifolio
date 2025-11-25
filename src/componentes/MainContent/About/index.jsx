e75940cf0a32c6be56e103269c99663aedb8b5e7
John Ávylan <avylanjohn@gmail.com>
2025-11-25 13:08:48 -0300

    Correção de bug de animação de texto

--- src/componentes/MainContent/About/index.jsx
+++ src/componentes/MainContent/About/index.jsx
@@ -17,7 +17,7 @@ export const About = () => {
     useSplitTextAbout(text2Ref)
 
     return (
-        <div id="about">
+        <div id="about" className="overflow-hidden">
             <div className="aboutSec flex justify-between items-center max-tablet:flex-col max-tablet:gap-8 max-tablet:pt-16">
                 <div ref={textRef} className="max-w-[60%] max-mobile:max-w-none max-mobile:text-center">
                     Estudante de Análise e Desenvolvimento de Sistemas, apaixonado por tecnologia e seu impacto na vida das pessoas. Tenho grande interesse em aprender e aprimorar minhas habilidades, sempre buscando novos desafios.
